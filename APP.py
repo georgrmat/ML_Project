e4708a9e1a11f9c096330b2a835fc394eb6516b4
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-03 22:19:29 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,3 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+
+df=pd.read_csv('heart.csv')
+st.dataframe(df)
+st.markdown("On propose en premier lieu de regarder quelques graphiques descriptive de notre base de donnée")
