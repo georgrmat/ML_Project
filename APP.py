c83f76ba2b529348eab0f8059d138fb6526b9d13
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-04 21:30:05 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -13,34 +13,34 @@ trestbps_target=pd.crosstab(df.trestbps,df.target)
 chol_target=pd.crosstab(df.trestbps,df.target)
 
 dica=age_target.to_dict()
-dfca=pd.concat({k: pd.Series(v) for k, v in dica.items()}).reset_index()
-dfca.columns = ['0_1', 'level','variable']
+dfa=pd.concat({k: pd.Series(v) for k, v in dica.items()}).reset_index()
+dfa.columns = ['0_1', 'level','variable']
 
 dictres=trestbps_target.to_dict()
-dfctres=pd.concat({k: pd.Series(v) for k, v in dictres.items()}).reset_index()
-dfctres.columns = ['0_1', 'level','variable']
+dftres=pd.concat({k: pd.Series(v) for k, v in dictres.items()}).reset_index()
+dftres.columns = ['0_1', 'level','variable']
 
 dicsex=sex_target.to_dict()
-dfcsex=pd.concat({k: pd.Series(v) for k, v in dicsex.items()}).reset_index()
-dfcsex.columns = ['0_1', 'level','variable']
+dfsex=pd.concat({k: pd.Series(v) for k, v in dicsex.items()}).reset_index()
+dfsex.columns = ['0_1', 'level','variable']
 
 diccol=trestbps_target.to_dict()
-dfccol=pd.concat({k: pd.Series(v) for k, v in diccol.items()}).reset_index()
-dfccol.columns = ['0_1', 'level','variable']
+dfcol=pd.concat({k: pd.Series(v) for k, v in diccol.items()}).reset_index()
+dfcol.columns = ['0_1', 'level','variable']
 
 
 
-gp_charta = alt.Chart(dfca).mark_bar().encode(
+gp_charta = alt.Chart(dfa).mark_bar().encode(
   alt.Column('0_1'), alt.X('level'),
   alt.Y('variable', axis=alt.Axis(grid=False)), 
-  alt.Color('0_1'))
+  alt.Color('level'))
 
-gp_charttres = alt.Chart(dfctres).mark_bar().encode(
+gp_charttres = alt.Chart(dftres).mark_bar().encode(
   alt.Column('0_1'), alt.X('level'),
   alt.Y('variable', axis=alt.Axis(grid=False)), 
-  alt.Color('0_1'))
+  alt.Color('level'))
 
-gp_charta = alt.Chart(dfsex).mark_bar().encode(
+gp_chartsex = alt.Chart(dfsex).mark_bar().encode(
   alt.Column('0_1'), alt.X('level'),
   alt.Y('variable', axis=alt.Axis(grid=False)), 
   alt.Color('level'))
