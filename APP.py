c275dd76a49b05943b1c0f6905fb662bc5ecf68c
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-05 01:02:07 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import altair as alt
+import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
