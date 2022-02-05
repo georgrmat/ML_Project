b45a1a6b38be7e2c89b648a68d02d4ac5c713962
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-05 15:34:42 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -11,68 +11,15 @@ df=pd.read_csv('heart.csv')
 st.dataframe(df)
 st.markdown("On propose en premier lieu de regarder quelques graphiques descriptive de notre base de donnée")
 
-# age_target=pd.crosstab(df['age'],df['target'])
-# sex_target=pd.crosstab(df.sex,df.target)
-# trestbps_target=pd.crosstab(df.trestbps,df.target)
-# chol_target=pd.crosstab(df.chol,df.target)
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
 
-# dica=age_target.to_dict()
-# dfa=pd.concat({k: pd.Series(v) for k, v in dica.items()}).reset_index()
-# dfa.columns = ['0_1', 'level','variable']
-
-# dictres=trestbps_target.to_dict()
-# dftres=pd.concat({k: pd.Series(v) for k, v in dictres.items()}).reset_index()
-# dftres.columns = ['0_1', 'level','variable']
-
-# dicsex=sex_target.to_dict()
-# dfsex=pd.concat({k: pd.Series(v) for k, v in dicsex.items()}).reset_index()
-# dfsex.columns = ['0_1', 'level','variable']
-
-# diccol=chol_target.to_dict()
-# dfcol=pd.concat({k: pd.Series(v) for k, v in diccol.items()}).reset_index()
-# dfcol.columns = ['0_1', 'level','variable']
-
-
-
-# gp_charta = alt.Chart(dfa).mark_bar().encode(
-#   alt.Column('0_1'), alt.X('level'),
-#   alt.Y('variable', axis=alt.Axis(grid=False)), 
-#   alt.Color('0_1'))
-
-# gp_chartcol = alt.Chart(dfcol).mark_bar().encode(
-#   alt.Column('0_1'), alt.X('level'),
-#   alt.Y('variable', axis=alt.Axis(grid=False)), 
-#   alt.Color('0_1'))
-
-
-# gp_charttres = alt.Chart(dftres).mark_bar().encode(
-#   alt.Column('0_1'), alt.X('level'),
-#   alt.Y('variable', axis=alt.Axis(grid=False)), 
-#   alt.Color('0_1'))
-
-# gp_chartsex = alt.Chart(dfsex).mark_bar().encode(
-#   alt.Column('0_1'), alt.X('level'),
-#   alt.Y('variable', axis=alt.Axis(grid=False)), 
-#   alt.Color('0_1'))
-
-
-
-# st.altair_chart(gp_charta, use_container_width=False)
-# st.altair_chart(gp_chartsex, use_container_width=False)
-# st.altair_chart(gp_chartcol, use_container_width=False)
-# st.altair_chart(gp_charttres, use_container_width=False)
-# plot_tab=pd.crosstab(df.age,df.target)
-
-# scater_age_col=alt.Chart(df).mark_point().encode(
-#     x='age',
-#     y='chol',
-#     color=alt.Color('target',  scale=alt.Scale(scheme='dark2'),legend=alt.Legend(title="age VS chol"))
-# )
-
-
-
-# #trestbps
-# st.altair_chart(scater_age_col, use_container_width=False)
+frames = [df, a, b, c]
+df = pd.concat(frames, axis = 1)
+df.head()
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+df.head()
 
 class variable (str):
   
@@ -101,15 +48,36 @@ class variable (str):
     if str=='The slope of the peak exercise ST segment':
       self.name='slope'
       
+     
+    
+class classifieur(str,par,X_trai,Y_train,X_test,Y_test):
+  
+  def __init(self,str)__:
+    if str=='KNeighbors'
+    self.algo=KNeighborsClassifier(n_neighbors = par)
+    if str=='Logistic Regression':
+      self.algo= LogisticRegression()
+    if str=='Support Vector Machine Algorithm':
+      self.algo=SVC(random_state = 1)
+    if str=='Naive Bayes Algorithm':
+      self.algo=GaussianNB() 
       
+    def train_classifieur(self,X_train,Y_train):
+      self.algo.fit(X_train,Y_train)
+    
+    def scor_classifieur(self):
+      return(self.algo.score(X_test.T,y_test.T)*100)
+      
+     
 
       
       
       
 var = st.radio(
      "What is the variable that you chose?  ",
-     ('Age', 'Cholesterol', 'Sex','Chest pain','Resting blood pressure','Fasting blood sugar','Resting electrocardiographic results','Maximum heart rate achieved',
-     'Exercise induced angina','ST depression induced by exercise relative to rest','The slope of the peak exercise ST segment'))
+     ('Age', 'Cholesterol','ST depression induced by exercise relative to rest','Resting blood pressure','Maximum heart rate achieved'))
+#, 'Sex','Chest pain','Resting blood pressure','Fasting blood sugar','Resting electrocardiographic results','Maximum heart rate achieved',
+ #    'Exercise induced angina','ST depression induced by exercise relative to rest','The slope of the peak exercise ST segment'))
 
 var_target=pd.crosstab(df[variable(var).name],df.target)
 
