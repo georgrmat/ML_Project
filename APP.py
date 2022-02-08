41c9161905805cac133142fc4dd5dbc13c237fff
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-08 19:51:00 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -127,8 +127,8 @@ class classifieur:#(str):#,par,X_trai,Y_train,X_test,Y_test):
     self.algo.fit(X_train,Y_train)
   
   
-  def get_parametrs(self):
-    param=self.
+#   def get_parametrs(self):
+#     param=self.
   
   def scor_classifieur(self,X_test,Y_test):
     return(self.algo.score(X_test,Y_test)*100)
