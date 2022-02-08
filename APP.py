2854de7dd866eb5fdfe9d4fff1ec28a677136fe6
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-08 23:58:32 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -105,7 +105,7 @@ class classifieur:#(str):#,par,X_trai,Y_train,X_test,Y_test):
     if str == 'Decision Tree':
       self.algo = DecisionTreeClassifier()
       self.grid_param = {'criterion':['gini','entropy'], 
-                         'max_depth': np.arange(3, 15)}
+                         'max_depth':[k for k in range(2,16)]}
       
     if str == 'Random Forest':
       self.algo = RandomForestClassifier()
