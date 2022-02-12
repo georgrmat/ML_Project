916b79337b507c8017147d78cdfb3d04f554d4a2
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-12 19:09:54 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -299,7 +299,7 @@ if choice_svsparam == 'Yes':
         params.append(modl.score(x_test, y_test))
         
       params_mean += np.array(params)
-    error={'par':u,'err': 1/N_mean*params_mean}
+    error={k:u,'err': 1/N_mean*params_mean}
     df_err=pd.DataFrame.from_dict(error, orient='columns', dtype=None, columns=None)
     base=alt.Chart(df_err)
     line11o = base.mark_line(color='#8A2BE2').encode(
